9b311eee169df6babc8db2eed7720555359cdc56
jean.depaula <jean.depaula@obraprimaweb.com.br>
2021-11-04 08:42:25 -0300

    Second Commit - Adicao da funcao que realiza a conversao da temperatura

--- Script/script.js
+++ Script/script.js
@@ -6,3 +6,31 @@ if(temperatureSelect.value == "c"){
     buttonConversor.textContent = "Converter para Celsius"
 }
 }
+
+function calcularTemperatura() {
+
+    let temperature = Number(firstTemperature.value)
+
+    if(temperatureSelect.value == "c"){
+        let Farenheit = temperature * 1.8 + 32
+        resultConversor.value = Farenheit
+    } else if(temperatureSelect.value == "f") {
+        let Celsius = (temperature - 32)/1.8
+        resultConversor.value = Celsius
+    }
+}
+
+buttonConversor.addEventListener("click", calcularTemperatura)
+
+function calcularTemperatura() {
+
+    let temperature = Number(firstTemperature.value)
+
+    if(temperatureSelect.value == "c"){
+        let Farenheit = temperature * 1.8 + 32
+        resultConversor.value = Farenheit.toFixed(2)
+    } else if(temperatureSelect.value == "f") {
+        let Celsius = (temperature - 32)/1.8
+        resultConversor.value = Celsius.toFixed(2)
+    }
+}
